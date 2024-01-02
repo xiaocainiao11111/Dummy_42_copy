ba52f807ce0eb47b7f3623b662068dc53d3aa02f
cainiao <321489189@qq.com>
2024-01-02 18:23:04 +0800

    cainiao

--- demo/P5.cbt6_cali/UserApp/main.cpp
+++ demo/P5.cbt6_cali/UserApp/main.cpp
@@ -1,21 +1,13 @@
 #include "common_inc.h"
 
-
-
-
 void tim4callback(void);
 
-
-
-
+uint8_t _a = 0;
 
 Test test;
 
-
-
 void Test::tick(void)
 {
-    
 }
 
 void Main()
@@ -33,6 +25,14 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
     if (htim == &htim4)
     {
         tim4callback();
+    }
+}
 
+void tim4callback()
+{
+    _a++;
+    if (_a == 100)
+    {
+        _a = 0;
     }
 }
